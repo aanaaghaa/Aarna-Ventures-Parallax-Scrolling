81e53d02aef944ab0f49e11202b275d85ae3175a
aanaaghaa <anagha2924@gmail.com>
2020-04-21 16:12:04 +0530

    init

---
+++ AarnaVentures/javascript/scripting.js
@@ -0,0 +1,42 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5e13622727773e0d832c1ef1/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    
+function home_hovered()        {document.getElementById("id_home").src="images/hover_home.png";}
+function about_hovered()        {document.getElementById("id_about").src="images/hover_about.png";}
+function divisions_hovered()    {document.getElementById("id_divisions").src="images/hover_divisions.png";}
+function contact_hovered()      {document.getElementById("id_phone").src="images/hover_phone.png";}
+
+function home_out()                 { document.getElementById("id_home").src="images/home.png";}
+function about_out()                 { document.getElementById("id_about").src="images/about.png";}
+function divisions_out()             { document.getElementById("id_divisions").src="images/divisions.png";}
+function contact_out()               { document.getElementById("id_phone").src="images/phone.png";} 
+
+function home_clicked()             {document.getElementById("id_home").src="images/onclick_home.png";}
+function about_clicked()             {document.getElementById("id_about").src="images/onclick_about.png";}
+function divisions_clicked()             {document.getElementById("id_divisions").src="images/onclick_divisions.png";}
+function contact_clicked()             {document.getElementById("id_phone").src="images/onclick_contact.png";}
+
+function fb_hovered()        {document.getElementById("id_fb").src="images/hover_facebook.png";}
+function insta_hovered()        {document.getElementById("id_insta").src="images/hover_instagram.png";}
+function linkedin_hovered()    {document.getElementById("id_linkedin").src="images/hover_linkedin.png";}
+function twitter_hovered()      {document.getElementById("id_twitter").src="images/hover_twitter.png";}
+function whatsapp_hovered()      {document.getElementById("id_whatsapp").src="images/hover_whatsapp.png";}
+
+function fb_out()                 { document.getElementById("id_fb").src="images/facebook.png";}
+function insta_out()                 { document.getElementById("id_insta").src="images/instagram.png";}
+function linkedin_out()             { document.getElementById("id_linkedin").src="images/linkedin.png";}
+function twitter_out()               { document.getElementById("id_twitter").src="images/twitter.png";} 
+function whatsapp_out()               { document.getElementById("id_whatsapp").src="images/whatsapp.png";} 
+
+function fb_clicked()             {  document.getElementById("id_fb").src="images/onclick_fb.png";  }
+function insta_clicked()             {document.getElementById("id_insta").src="images/onclick_instagram.png";}
+function linkedin_clicked()             {document.getElementById("id_linkedin").src="images/onclick_linkedin.png";}
+function twitter_clicked()             {document.getElementById("id_twitter").src="images/onclick_twitter.png";}
+function whatsapp_clicked()             {document.getElementById("id_whatsapp").src="images/onclick_whatsapp.png";}
